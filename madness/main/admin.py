dc3f279e71798f2739e2b86c4aeab37e07a68048
ArikuWoW <91748313+ArikuWoW@users.noreply.github.com>
2025-01-23 16:19:02 +0300

    Доделываю корзину

---
+++ madness/main/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Size, Category, \
+    ClothingItem, ClothingItemSize, ItemImage
+    
+    
+class ClothingItemSizeInLine(admin.TabularInline):
+    model = ClothingItemSize
+    extra = 4
+    
+class ItemImageInLine(admin.TabularInline):
+    model = ItemImage
+    extra = 4
+    
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+    
+@admin.register(Category)
+class cATEGORYAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    
+
+@admin.register(ClothingItem)
+class ClothingItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category',
+                    'available', 'price', 'discount',
+                    'created_at', 'updated_at')
+    list_filter = ('available', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-created_at',)
+    inlines = [ClothingItemSizeInLine, ItemImageInLine]
